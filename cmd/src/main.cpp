f277e20c41df4edec7169c09911cc8c67e1565e8
Greg Wuller <greg@afofo.com>
2021-12-11 14:39:25 -0800

    Add argument parser

--- cmd/src/main.cpp
+++ cmd/src/main.cpp
@@ -1,7 +1,33 @@
-#include "utu/utu.h"
+#include <iostream>
 
-int main(int argc, char* argv[])
+#include "docopt.h"
+#include "utu/utu.h"
+#include "utu/version.h"
+
+static const char USAGE[] =
+    R"(utu
+
+    Usage:
+      utu analyze <audio_file>
+      utu synth <partials_file>
+      utu (-h | --help)
+      utu --version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+)";
+
+int main(int argc, const char** argv)
 {
-  utu::herald();
+  std::map<std::string, docopt::value> args = docopt::docopt(USAGE, {argv + 1, argv + argc},
+                                                             true,  // show help if requested
+                                                             PROJECT_VERSION);  // version string
+
+  for (auto const& arg : args)
+  {
+    std::cout << arg.first << arg.second << std::endl;
+  }
+
   return 0;
 }
